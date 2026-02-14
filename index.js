bdd3cb4066a4ead8113201abd1f5f72414d3cf83
Anisha Chhajer <anishachhajer16@gmail.com>
2026-02-14 10:27:44 +0530

    api with nodejs

--- index.js
+++ index.js
@@ -1,167 +1,69 @@
 
 
-const express = require("express");
-const app = express();
-app.use(express.json());
-
-const cg = [
-{
-"id": 1,
-"title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-"price": 109.95,
-"category": "men's clothing",
-"image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
-"rating": {
-"rate": 3.9,
-"count": 120
-}
-},
-{
-"id": 2,
-"title": "Mens Casual Premium Slim Fit T-Shirts ",
-"price": 22.3,
-"category": "men's clothing",
-"image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png",
-"rating": {
-"rate": 4.1,
-"count": 259
-}
-},
-{
-"id": 3,
-"title": "Cotton Jacket",
-"price": 55.99,
-"category": "women's clothing",
-"image": "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_t.png",
-"rating": {
-"rate": 4.7,
-"count": 500
-}
-}];
-
-app.get("/", (req, res) => {
-  res.send("Express server is running");
-});
-
-app.get("/cg", (req, res) => {
-  res.status(200).json(cg);
-});
-
-app.get("/cg/students", (req, res) => {
-  const names = cg.map((student) => student.studentName);
-  res.status(200).json(names);
-});
-
-
-
-app.get("/cg/product/:id", (req, res) => {
-  const { id } = req.params;
-
-  const student = cg.find(
-    (s) => s.id === parseInt(id)
-  );
-
-  if (!student) {
-    return res.status(404).json({
-      message: "Student not found",
-    });
-  }
-
-  res.status(200).json(student);
-});
-
-
-app.get("/cg/category/:type", (req, res) => {
-  const { type } = req.params;
-
-  const student = cg.find(
-    (s) => s.category === type
-  );
-
-  if (!student) {
-    return res.status(404).json({
-      message: "Student not found",
-    });
-  }
-
-  res.status(200).json(student);
-});
-
-
-app.post("/cg/product/:id", (req, res) => {
-  const { id } = req.params;
-
-  const student = cg.find(
-    (s) => s.id === parseInt(id)
-  );
-
-  if (!student) {
-    return res.status(404).json({
-      message: "Student not found",
-    });
-  }
-
-  res.status(200).json(student);
-});
-
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
-});
-
-
 // const express = require("express");
 // const app = express();
+// app.use(express.json());
 
-// app.use(express.json()); 
-
-// let products = [
-//   {
-//     id: 1,
-//     title: "Fjallraven - Backpack",
-//     price: 109.95,
-//     category: "men's clothing",
-//     image: "image1.png",
-//     rating: { rate: 3.9, count: 120 }
-//   },
-//   {
-//     id: 2,
-//     title: "Mens Casual T-Shirt",
-//     price: 22.3,
-//     category: "men's clothing",
-//     image: "image2.png",
-//     rating: { rate: 4.1, count: 259 }
-//   },
-//   {
-//     id: 3,
-//     title: "Cotton Jacket",
-//     price: 55.99,
-//     category: "women's clothing",
-//     image: "image3.png",
-//     rating: { rate: 4.7, count: 500 }
-//   }
-// ];
+// const products = [
+//     {
+//         "id": 1,
+//         "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+//         "price": 109.95,
+//         "category": "men's clothing",
+//         "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
+//         "rating": {
+//             "rate": 3.9,
+//             "count": 120
+//         }
+//     },
+//     {
+//         "id": 2,
+//         "title": "Mens Casual Premium Slim Fit T-Shirts ",
+//         "price": 22.3,
+//         "category": "men's clothing",
+//         "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png",
+//         "rating": {
+//             "rate": 4.1,
+//             "count": 259
+//         }
+//     },
+//     {
+//         "id": 3,
+//         "title": "Cotton Jacket",
+//         "price": 55.99,
+//         "category": "women's clothing",
+//         "image": "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_t.png",
+//         "rating": {
+//             "rate": 4.7,
+//             "count": 500
+//         }
+//     }];
 
 // app.get("/all", (req, res) => {
 //   res.json(products);
 // });
 
-// app.get("/product/:id", (req, res) => {
-//   const id = parseInt(req.params.id);
-//   const product = products.find(p => p.id === id);
+// app.get("/products/:id", (req, res) => {
+//     const { id } = req.params;
+//     const product = products.find(
+//         (p) => p.id === parseInt(id)
+//     );
+//     if (!product) {
+//         return res.status(404).json({
+//             message: "Product not found",
+//         });
+//     }
+//     res.status(200).json(product);
+// });cd 
 
-//   if (!product) {
-//     return res.status(404).json({ message: "Product not found" });
-//   }
-
-//   res.json(product);
+// app.get("/products", (req, res) => {
+//     res.status(200).json(products);
 // });
 
-// app.post("/product/:id", (req, res) => {
+// app.post("/product", (req, res) => {
 //   const newProduct = req.body;
 
-//   if (!newProduct.id || !newProduct.title) {
-//     return res.status(400).json({ message: "Invalid product data" });
-//   }
+//   newProduct.id = products.length + 1;
 
 //   products.push(newProduct);
 
@@ -171,21 +73,151 @@ app.listen(3000, () => {
 //   });
 // });
 
+
 // app.get("/category/:type", (req, res) => {
-//   const type = req.params.type.toLowerCase();
+//     const { type } = req.params;
+//     const filteredProducts = products.filter(
+//         (p) => p.category === type
+//     );
+//     if (!filteredProducts.length) {
+//         return res.status(404).json({
+//             message: "No products found in this category",
+//         });
+//     }
+//     res.status(200).json(filteredProducts);
+// });
 
-//   const filteredProducts = products.filter(
-//     p => p.category.toLowerCase() === type
-//   );
 
-//   if (filteredProducts.length === 0) {
-//     return res.status(404).json({ message: "No products found" });
+// app.post("/products", (req, res) => {
+//   const newProducts = req.body; 
+
+//   if (!Array.isArray(newProducts)) {
+//     return res.status(400).json({
+//       message: "Please send an array of products"
+//     });
 //   }
 
-//   res.json(filteredProducts);
-// });
+//   newProducts.forEach(product => {
+//     product.id = products.length + 1;
+//     products.push(product);
+//   });
 
+//   res.status(201).json({
+//     message: "Multiple products added successfully",
+//     totalProducts: products.length
+//   });
+// });
 
 // app.listen(3000, () => {
-//   console.log("Server running on http://localhost:3000");
+//     console.log("Server started on port 3000");
 // });
+
+
+
+
+
+
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+//in memory database
+//data stored in RAM
+//lifespan:-till server is on
+const products = [
+    {
+        "id": 1,
+        "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+        "price": 109.95,
+        "category": "men's clothing",
+        "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
+        "rating": {
+            "rate": 3.9,
+            "count": 120
+        }
+    },
+    {
+        "id": 2,
+        "title": "Mens Casual Premium Slim Fit T-Shirts ",
+        "price": 22.3,
+        "category": "men's clothing",
+        "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png",
+        "rating": {
+            "rate": 4.1,
+            "count": 259
+        }
+    },
+    {
+        "id": 3,
+        "title": "Cotton Jacket",
+        "price": 55.99,
+        "category": "women's clothing",
+        "image": "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_t.png",
+        "rating": {
+            "rate": 4.7,
+            "count": 500
+        }
+    }];
+
+app.get("/all", (req, res) => {
+  res.json(products);
+});
+
+
+app.get("/product/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const product = products.find(p => p.id === id);
+
+  if (!product) {
+    return res.status(404).json({ message: "Product not found" });
+  }
+
+  res.json(product);
+});
+
+app.post("/product", (req, res) => {
+  const newProduct = req.body;
+
+  newProduct.id = products.length + 1;
+
+  products.push(newProduct);
+
+  res.status(201).json({
+    message: "Product added successfully",
+    product: newProduct
+  });
+});
+
+
+app.get("/category/:type", (req, res) => {
+  const type = req.params.type;
+
+  const filteredProducts = products.filter(p => p.category.toLowerCase() === type.toLowerCase());
+  res.json(filteredProducts);
+});
+
+app.post("/products", (req, res) => {
+  const newProducts = req.body; 
+  if (!Array.isArray(newProducts)) {
+    return res.status(400).json({
+      message: "Send array of products"
+    });
+}
+
+  newProducts.forEach(product => {
+    product.id = products.length + 1;
+    products.push(product);
+  });
+
+  res.status(201).json({
+    message: "Multiple products added successfully",
+    totalProducts: products.length
+  });
+});
+
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
